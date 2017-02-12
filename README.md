4685ed05331e5f9db0bb696d33f31243e751f3cb
G-JE <gunnarjerickson@gmail.com>
2017-02-12 14:37:17 -0500

    Full package

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Reacting
+#In the reducers each is a function that acts a key to an object that holds the data for action creators to update state. The name of the key
+# is set by the import name and variable name sent to the combineReducer function.
--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,12 @@ module.exports = {
     devServer: {
         inline: true,
         contentBase: './src',
-        port: 3000
+        port: 4900,
+        historyApiFallback: true,
     },
+
     devtool: ['cheap-module-eval-source-map', 'inline-source-map'],
-    entry: './src/app/index.js',
+    entry: './src/index.js',
     module: {
         loaders: [
             {
@@ -16,12 +18,24 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'babel',
                 query: {
-                  presets:['react','es2015']
+                    presets: ['react', 'es2015']
                 }
             },
             {
-                test: /\.scss/,
+                test: /\.scss$/,
                 loader: 'style-loader!css-loader!sass-loader'
+            },
+            {
+                test: /\.(eot|otf|svg|ttf|woff|woff2)$/,
+                loader: 'file?name=fonts/[name].[ext]'
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "file-loader?name=fonts/[name].[ext]"
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loader: "file-loader?name=Images/[name].[ext]"
             }
         ]
     },
@@ -30,6 +44,10 @@ module.exports = {
         filename: 'bundle.min.js',
     },
     plugins: [
-        new webpack.optimize.OccurrenceOrderPlugin()
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.ProvidePlugin({
+            'd3': 'd3',
+            'React': 'react'
+        })
     ]
 };
